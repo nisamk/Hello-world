0a0032492bf3a58bb23bb5e41fdbcc613902685c
nisamk <nisamkari@gmail.com>
2016-08-23 15:06:16 +0530

    changed

--- a.py
+++ a.py
@@ -1,12 +1,7 @@
 file = open("newfile.txt", "w")
-
 file.write("hello world in the new file")
-
 file.write("and another line")
 file = open('newfile.txt', 'r')
-
 print file.read()
-
-
 file.close()
 
