3125a5f9fcb4c3293129afdef7b1d8f46263ff4c
nisamk <nisamkari@gmail.com>
2016-08-23 15:16:31 +0530

    adding

---
+++ DIC.py
@@ -0,0 +1,9 @@
+"""ad={}
+ad['nisam']="nisam@gmaiol.com"
+ad["nithin"]="nithin@gmail.com"
+ad["sachin"]="sachin@gmail.com"
+nz={'njan':'njan@gmail.com','polichuy':'polichu@gmail.com'}
+print(ad.values())
+print(nz.keys())""" 
+print(chr(65))
+
